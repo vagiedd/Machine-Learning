84551f107746cbc2e20b37ace9b71ccec0f5509c
vagiedd <72588499+vagiedd@users.noreply.github.com>
2021-02-23 11:43:55 -0600

    Update main.m

--- ex1/main.m
+++ ex1/main.m
@@ -33,6 +33,7 @@ predict1 = [1, 3.5] *theta;
 theta0_vals = linspace(-10, 10, 100);
 theta1_vals = linspace(-1, 4, 100);
 
+
 % initialize J_vals to a matrix of 0's
 J_vals = zeros(length(theta0_vals), length(theta1_vals));
 
